061962bce50f3dbca1a5a983422d4b1bed1cb9dc
Med Elmostapha <dedijekani@gmail.com>
2026-02-16 22:49:29 +0000

    feat: implement Category model with user ownership

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 # Register your models here.
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import User,Category
 
 admin.site.register(User, UserAdmin)
+
+admin.site.register(Category)
--- accounts/models.py
+++ accounts/models.py
@@ -10,3 +10,22 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+from django.conf import settings
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='categories'
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.owner.username}"
