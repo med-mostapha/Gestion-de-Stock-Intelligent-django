faeffb4e2fb8976e54152ed9ed1cfaff91f748d8
Med Elmostapha <dedijekani@gmail.com>
2026-02-16 23:19:36 +0000

    feat: implement user registration API using DRF

---
+++ accounts/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from .models import User
+
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'phone', 'password']
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data.get('email'),
+            phone=validated_data.get('phone'),
+            password=validated_data['password']
+        )
+        return user
